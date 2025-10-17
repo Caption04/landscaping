e276629671f71ff31caba72981d5e8c6c225f663
Caption04 <kgkatunga04@gmail.com>
2025-10-17 12:42:46 +0200

    green

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 const header = document.querySelector(".header");
 const menu = document.querySelector(".menu");
-const navButtons = document.querySelectorAll('.nav-menu-btn, .small-hero-btn');
+const navButton = document.querySelector(".nav-menu-btn");
 
-navButton.forEach(button => {
-  button.addEventListener("click", (e) => {
-      const clickedBtn = e.currentTarget;
-      console.log('Clicked', clickedBtn);
-      const closeBtn = button.querySelector(".menu-close-btn");
-      const openBtn = button.querySelector(".menu-open-btn");
+navButton.addEventListener("click", () => {
+    const closeBtn = navButton.querySelector(".menu-close-btn");
+    const openBtn = navButton.querySelector(".menu-open-btn");
 
-      menu.classList.toggle("open");
-      openBtn.classList.toggle("open-btn");
-      closeBtn.classList.toggle("close-btn");
-      if(menu.classList.contains("open")){
-          document.body.style.overflow = "hidden";
-      }
-      else{
-          document.body.style.overflow = "auto";
-      }
-  })
+    menu.classList.toggle("open");
+    openBtn.classList.toggle("open-btn");
+    closeBtn.classList.toggle("close-btn");
+    if(menu.classList.contains("open")){
+        document.body.style.overflow = "hidden";
+    }
+    else{
+        document.body.style.overflow = "auto";
+    }
 })
 
+const smallBtn = document.querySelector(".small-hero-btn");
+smallBtn.addEventListener("click", () => {
+    const closeBtn = navButton.querySelector(".menu-close-btn");
+    const openBtn = navButton.querySelector(".menu-open-btn");
+
+    menu.classList.toggle("open");
+    openBtn.classList.toggle("open-btn");
+    closeBtn.classList.toggle("close-btn");
+    if(menu.classList.contains("open")){
+        document.body.style.overflow = "hidden";
+    }
+    else{
+        document.body.style.overflow = "auto";
+    }
+})
 
 // const images = document.querySelectorAll('.gallery-container img');
 // let currentIndex = 0; // tracks which image is currently on top
